0379a3ac1e080096ccfc6d17e9976ef2c550adf2
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-08-30 14:42:07 +0530

    Created slides list

---
+++ slides.ts
@@ -0,0 +1,32 @@
+import { Image } from "react-native-svg";
+
+type Slides = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+};
+const slides: Slides[] = [
+  {
+    id: 1,
+    title: "Welcome to Vegatise",
+    description:
+      "A Platform To Earn Income Passively By Partnering With Brands",
+    image: require("../assets/images/slide1.png"),
+  },
+  {
+    id: 2,
+    title: "Get Paid",
+    description:
+      "A Platform To Earn Income Passively By Partnering With Brands",
+    image: require("../assets/images/slide1.png"),
+  },
+  {
+    id: 3,
+    title: "Explore",
+    description:
+      "A Platform To Earn Income Passively By Partnering With Brands",
+    image: require("../assets/images/slide1.png"),
+  },
+];
+export default slides;
