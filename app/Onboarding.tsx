7d3d6874a47f761ad66837295acf764b6b0aed42
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-08-30 14:42:53 +0530

    Created Onboarding

---
+++ app/Onboarding.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  View,
+} from "react-native";
+import { Paragraph, XStack, YStack } from "tamagui";
+import slides from "slides";
+import OnboardingItem from "./OnboardingItem";
+
+const { width, height } = Dimensions.get("window");
+
+export default function Onboarding() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={slides}
+        renderItem={({ item }) => <OnboardingItem item={item} />}
+        // horizontal
+        // showsHorizontalScrollIndicator
+        // pagingEnabled
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
