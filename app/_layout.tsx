219f6deae6206cf046ac89998e2e86fbc7d21a4c
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-08-31 15:56:05 +0530

    feat: add Poppins as default font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,25 @@ import '../tamagui-web.css'
 import { useEffect } from 'react'
 import { useColorScheme } from 'react-native'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
 import { Provider } from './Provider'
+import {
+  Poppins_300Light,
+  Poppins_300Light_Italic,
+  Poppins_400Regular,
+  Poppins_400Regular_Italic,
+  Poppins_500Medium,
+  Poppins_500Medium_Italic,
+  Poppins_600SemiBold,
+  Poppins_600SemiBold_Italic,
+  Poppins_700Bold,
+  Poppins_700Bold_Italic,
+  Poppins_800ExtraBold,
+  Poppins_800ExtraBold_Italic,
+  Poppins_900Black,
+  Poppins_900Black_Italic,
+  useFonts,
+} from '@expo-google-fonts/poppins'
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,19 +37,33 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const [interLoaded, interError] = useFonts({
-    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  const [fontLoaded, fontError] = useFonts({
+    // Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    // InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    Poppins300: Poppins_300Light,
+    Poppins300Italic: Poppins_300Light_Italic,
+    Poppins400: Poppins_400Regular,
+    Poppins400Italic: Poppins_400Regular_Italic,
+    Poppins500: Poppins_500Medium,
+    Poppins500Italic: Poppins_500Medium_Italic,
+    Poppins600: Poppins_600SemiBold,
+    Poppins600Italic: Poppins_600SemiBold_Italic,
+    Poppins700: Poppins_700Bold,
+    Poppins700Italic: Poppins_700Bold_Italic,
+    Poppins800: Poppins_800ExtraBold,
+    Poppins800Italic: Poppins_800ExtraBold_Italic,
+    Poppins900: Poppins_900Black,
+    Poppins900Italic: Poppins_900Black_Italic,
   })
 
   useEffect(() => {
-    if (interLoaded || interError) {
+    if (fontLoaded || fontError) {
       // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
       SplashScreen.hideAsync()
     }
-  }, [interLoaded, interError])
+  }, [fontLoaded, fontError])
 
-  if (!interLoaded && !interError) {
+  if (!fontLoaded && !fontError) {
     return null
   }
 
@@ -47,6 +77,13 @@ function RootLayoutNav() {
     <Provider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+
           <Stack.Screen
             name="(tabs)"
             options={{
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,12 +1,51 @@
-import { config as configBase } from '@tamagui/config/v3'
-import { createTamagui } from 'tamagui'
+import { animations, config, shorthands } from '@tamagui/config/v3'
+import { themes, tokens } from '@tamagui/themes'
+import { createFont, createTamagui } from 'tamagui'
 
-export const config = createTamagui(configBase)
+const poppinsFace = {
+  normal: { normal: 'Poppins400', italic: 'Poppins400Italic' },
+  300: { normal: 'Poppins300', italic: 'Poppins300Italic' },
+  500: { normal: 'Poppins500', italic: 'Poppins500Italic' },
+  600: { normal: 'Poppins600', italic: 'Poppins600Italic' },
+  700: { normal: 'Poppins700', italic: 'Poppins700Italic' },
+  800: { normal: 'Poppins800', italic: 'Poppins800Italic' },
+  900: { normal: 'Poppins900', italic: 'Poppins900Italic' },
+}
 
-export default config
+const headingFont = createFont({
+  size: config.fonts.heading.size,
+  lineHeight: config.fonts.heading.lineHeight,
+  weight: config.fonts.heading.weight,
+  letterSpacing: config.fonts.heading.letterSpacing,
+  face: poppinsFace,
+})
 
-export type Conf = typeof config
+const bodyFont = createFont({
+  size: config.fonts.body.size,
+  lineHeight: config.fonts.body.lineHeight,
+  weight: config.fonts.body.weight,
+  letterSpacing: config.fonts.body.letterSpacing,
+  face: poppinsFace,
+})
+
+export const appConfig = createTamagui({
+  animations,
+  shorthands,
+  fonts: {
+    heading: headingFont,
+    body: bodyFont,
+  },
+  themes,
+  tokens,
+})
+
+// Default config
+// export const appConfig = createTamagui(config)
+
+export type AppConfig = typeof appConfig
 
 declare module 'tamagui' {
-  interface TamaguiCustomConfig extends Conf {}
+  interface TamaguiCustomConfig extends AppConfig {}
 }
+
+export default appConfig
