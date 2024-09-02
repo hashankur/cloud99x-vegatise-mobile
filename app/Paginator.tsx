f7d32cf28336f46084161582f6d2e79ad2c6b206
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-09-02 13:27:59 +0530

    Paginator fixed

--- app/Paginator.tsx
+++ app/Paginator.tsx
@@ -1,32 +1,38 @@
-import { View, StyleSheet, useWindowDimensions } from "react-native";
+import {
+  View,
+  StyleSheet,
+  useWindowDimensions,
+  Text,
+  Animated,
+} from "react-native";
 import React from "react";
-import { Container, Key } from "@tamagui/lucide-icons";
-import { Extrapolation } from "react-native-reanimated";
+// import { Container, Key } from "@tamagui/lucide-icons";
+// import { Extrapolation } from "react-native-reanimated";
 
 export default Paginator = ({ data, scrollX }) => {
   const { width } = useWindowDimensions();
   return (
-    <View style={{ flexDirection: "row", height: 64 }}>
-      {data.map((_, i) => {
-        const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-        const dotWidth = scrollX.interpolate({
-          inputRange,
-          outputRange: [10, 20, 10],
-          extrapolate: "clamp",
-        });
-        const opacity = scrollX.interpolate({
-          inputRange,
-          outputRange: [0.3, 1, 0.3],
-          extrapolate: "clamp",
-        });
-
-        return (
-          <View
-            style={[styles.dot, { width: dotWidth, opacity }]}
-            key={i.toString()}
-          />
-        );
-      })}
+    <View>
+      <View style={{ flexDirection: "row", height: 64 }}>
+        {data.map((_, i) => {
+          const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+          const dotWidth = scrollX.interpolate({
+            inputRange,
+            outputRange: [10, 20, 10],
+            extrapolate: "clamp",
+            // });
+            // const opacity = scrollX.interpolate({
+            //   inputRange,
+          });
+          return (
+            <Animated.View
+              style={[styles.dot, { width: dotWidth }]}
+              key={i.toString()}
+            />
+          );
+        })}
+      </View>
+      <Text style={styles.text}>Skip</Text>
     </View>
   );
 };
@@ -37,5 +43,12 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: "#D33A3A",
     marginHorizontal: 5,
+  },
+  text: {
+    fontWeight: "100",
+    fontSize: 15,
+    marginBottom: 10,
+    paddingTop: 13,
+    // textAlign: "center",
   },
 });
