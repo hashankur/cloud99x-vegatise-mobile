38dc314c533125b2fda573894dce53876e10cd80
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-09-10 14:44:01 +0530

    feat: add Island Tabs component

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -20,133 +20,120 @@ import IslandTabs from '../../components/ui/IslandTabs'
 
 export default function LoginScreen() {
   return (
-    <>
-      <YStack
-        flex={1}
-        alignItems="center"
-        gap="$5"
-        px="$5"
-        pt="$5"
-        backgroundColor="white"
-      >
-        <Image source={logo} width={200} height={150} objectFit="contain" />
-        <H4 alignSelf="flex-start">Welcome To, Vegatise</H4>
+    <YStack flex={1} alignItems="center" gap="$5" px="$5" pt="$5" backgroundColor="white">
+      <Image source={logo} width={200} height={150} objectFit="contain" />
+      <H4 alignSelf="flex-start">Welcome To, Vegatise</H4>
 
-        <Tabs defaultValue="tab1" flexDirection="column" w="100%">
-          <Tabs.List alignSelf="center" mb="$6">
-            <Tabs.Tab flex={1} value="tab1">
-              <SizableText>Phone Number</SizableText>
-            </Tabs.Tab>
-            <Tabs.Tab flex={1} value="tab2">
-              <SizableText>Email</SizableText>
-            </Tabs.Tab>
-          </Tabs.List>
+      <IslandTabs tabs={['Phone Number', 'Email']} />
 
-          {/* Tab Section - Phone Number */}
-          <Tabs.Content value="tab1">
-            <SizableText fontSize="$2" color="$label" mb="$1" fontWeight={600}>
-              Phone Number
+      <Tabs defaultValue="tab1" flexDirection="column" w="100%">
+        <Tabs.List alignSelf="center" mb="$6">
+          <Tabs.Tab flex={1} value="tab1">
+            <SizableText>Phone Number</SizableText>
+          </Tabs.Tab>
+          <Tabs.Tab flex={1} value="tab2">
+            <SizableText>Email</SizableText>
+          </Tabs.Tab>
+        </Tabs.List>
+
+        {/* Tab Section - Phone Number */}
+        <Tabs.Content value="tab1">
+          <SizableText fontSize="$2" color="$label" mb="$1" fontWeight={600}>
+            Phone Number
+          </SizableText>
+
+          <XStack gap="$2">
+            <Select defaultValue="">
+              <Select.Trigger
+                width="$10"
+                iconAfter={<Iconify icon="mdi:chevron-down" size={24} />}
+              >
+                <Select.Value placeholder="+94" />
+              </Select.Trigger>
+
+              <Adapt when="sm" platform="touch">
+                {/* or <Select.Sheet> */}
+                <Sheet>
+                  <Sheet.Frame>
+                    <Adapt.Contents />
+                  </Sheet.Frame>
+                  <Sheet.Overlay />
+                </Sheet>
+              </Adapt>
+
+              <Select.Content>
+                <Select.ScrollUpButton />
+                <Select.Viewport>
+                  <Select.Group>
+                    <Select.Label />
+                    <Select.Item>
+                      <Select.ItemText />
+                    </Select.Item>
+                  </Select.Group>
+                </Select.Viewport>
+                <Select.ScrollDownButton />
+              </Select.Content>
+            </Select>
+
+            <Input flex={1} placeholder="769016274" />
+          </XStack>
+
+          <Button
+            backgroundColor="$btn"
+            color="white"
+            fontWeight={600}
+            mt="$6"
+            onPress={() => {
+              router.navigate('/login/otp')
+            }}
+          >
+            Request OTP
+          </Button>
+        </Tabs.Content>
+
+        {/* Tab Section - Email */}
+        <Tabs.Content value="tab2" gap="$5">
+          <XStack flexWrap="wrap">
+            <Label
+              fontSize="$2"
+              color="$label"
+              fontWeight={600}
+              width="100%"
+              htmlFor="email"
+            >
+              Email Address
+            </Label>
+            <Input w="100%" id="email" placeholder="email@domain.com" />
+          </XStack>
+
+          <XStack flexWrap="wrap">
+            <Label
+              fontSize="$2"
+              color="$label"
+              fontWeight={600}
+              width="100%"
+              htmlFor="password"
+            >
+              Password
+            </Label>
+            <Input w="100%" id="password" />
+            <SizableText w="100%" pt="$2" textAlign="right" fontSize="$2" color="$link">
+              <Link href="/forgot-password">Forgot Password?</Link>
             </SizableText>
+          </XStack>
 
-            <XStack gap="$2">
-              <Select defaultValue="">
-                <Select.Trigger
-                  width="$10"
-                  iconAfter={<Iconify icon="mdi:chevron-down" size={24} />}
-                >
-                  <Select.Value placeholder="+94" />
-                </Select.Trigger>
+          <Button disabled backgroundColor="$btn" color="white" fontWeight={600} mt="$2">
+            Login
+          </Button>
+        </Tabs.Content>
+      </Tabs>
 
-                <Adapt when="sm" platform="touch">
-                  {/* or <Select.Sheet> */}
-                  <Sheet>
-                    <Sheet.Frame>
-                      <Adapt.Contents />
-                    </Sheet.Frame>
-                    <Sheet.Overlay />
-                  </Sheet>
-                </Adapt>
-
-                <Select.Content>
-                  <Select.ScrollUpButton />
-                  <Select.Viewport>
-                    <Select.Group>
-                      <Select.Label />
-                      <Select.Item>
-                        <Select.ItemText />
-                      </Select.Item>
-                    </Select.Group>
-                  </Select.Viewport>
-                  <Select.ScrollDownButton />
-                </Select.Content>
-              </Select>
-
-              <Input flex={1} placeholder="769016274" />
-            </XStack>
-
-            <Button
-              backgroundColor="$btn"
-              color="white"
-              fontWeight={600}
-              mt="$6"
-              onPress={() => {
-                router.navigate('/login/otp')
-              }}
-            >
-              Request OTP
-            </Button>
-          </Tabs.Content>
-
-          {/* Tab Section - Email */}
-          <Tabs.Content value="tab2" gap="$5">
-            <XStack flexWrap="wrap">
-              <Label
-                fontSize="$2"
-                color="$label"
-                fontWeight={600}
-                width="100%"
-                htmlFor="email"
-              >
-                Email Address
-              </Label>
-              <Input w="100%" id="email" placeholder="email@domain.com" />
-            </XStack>
-
-            <XStack flexWrap="wrap">
-              <Label
-                fontSize="$2"
-                color="$label"
-                fontWeight={600}
-                width="100%"
-                htmlFor="password"
-              >
-                Password
-              </Label>
-              <Input w="100%" id="password" />
-              <SizableText w="100%" pt="$2" textAlign="right" fontSize="$2" color="$link">
-                <Link href="/forgot-password">Forgot Password?</Link>
-              </SizableText>
-            </XStack>
-
-            <Button
-              disabled
-              backgroundColor="$btn"
-              color="white"
-              fontWeight={600}
-              mt="$2"
-            >
-              Login
-            </Button>
-          </Tabs.Content>
-        </Tabs>
-
-        <YStack alignItems="center" mt="$3">
-          <Paragraph>Not Registered Yet?</Paragraph>
-          <Paragraph color="red" fontWeight={500}>
-            <Link href="/">Create An Account</Link>
-          </Paragraph>
-        </YStack>
+      <YStack alignItems="center" mt="$3">
+        <Paragraph>Not Registered Yet?</Paragraph>
+        <Paragraph color="red" fontWeight={500}>
+          <Link href="/">Create An Account</Link>
+        </Paragraph>
       </YStack>
-    </>
+    </YStack>
   )
 }
---
+++ components/ui/IslandTabs.tsx
@@ -0,0 +1,176 @@
+import React from 'react'
+import type { StackProps, TabLayout, TabsTabProps } from 'tamagui'
+import { AnimatePresence, H5, SizableText, Tabs, YStack, styled } from 'tamagui'
+
+export default function IslandTabs({}) {
+  const [tabState, setTabState] = React.useState<{
+    currentTab: string
+    /**
+     * Layout of the Tab user might intend to select (hovering / focusing)
+     */
+    intentAt: TabLayout | null
+    /**
+     * Layout of the Tab user selected
+     */
+    activeAt: TabLayout | null
+    /**
+     * Used to get the direction of activation for animating the active indicator
+     */
+    prevActiveAt: TabLayout | null
+  }>({
+    activeAt: null,
+    currentTab: 'tab1',
+    intentAt: null,
+    prevActiveAt: null,
+  })
+
+  const setCurrentTab = (currentTab: string) => setTabState({ ...tabState, currentTab })
+  const setIntentIndicator = (intentAt) => setTabState({ ...tabState, intentAt })
+  const setActiveIndicator = (activeAt) =>
+    setTabState({ ...tabState, prevActiveAt: tabState.activeAt, activeAt })
+  const { activeAt, intentAt, prevActiveAt, currentTab } = tabState
+
+  // 1 = right, 0 = nowhere, -1 = left
+  const direction = (() => {
+    if (!activeAt || !prevActiveAt || activeAt.x === prevActiveAt.x) {
+      return 0
+    }
+    return activeAt.x > prevActiveAt.x ? -1 : 1
+  })()
+
+  const handleOnInteraction: TabsTabProps['onInteraction'] = (type, layout) => {
+    if (type === 'select') {
+      setActiveIndicator(layout)
+    } else {
+      setIntentIndicator(layout)
+    }
+  }
+
+  return (
+    <Tabs
+      value={currentTab}
+      onValueChange={setCurrentTab}
+      orientation="horizontal"
+      size="$4"
+      padding="$2"
+      height={150}
+      flexDirection="column"
+      activationMode="manual"
+      backgroundColor="white" // entire
+      borderRadius="$4"
+      // position="relative"
+      w="100%"
+    >
+      <YStack>
+        <Tabs.List
+          disablePassBorderRadius
+          loop={false}
+          aria-label="Manage your account"
+          gap="$2"
+          backgroundColor="$bgAlt" // Tab group bg
+          p="$1.5"
+        >
+          {/* Order of active and intent indicator matters relative to the tabs */}
+          <AnimatePresence>
+            {intentAt && (
+              <TabsRovingIndicator
+                borderRadius="$4"
+                width={intentAt.width}
+                height={intentAt.height}
+                x={intentAt.x}
+                y={intentAt.y}
+              />
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {activeAt && (
+              <TabsRovingIndicator
+                borderRadius="$4"
+                theme="active"
+                width={activeAt.width}
+                height={activeAt.height}
+                x={activeAt.x}
+                y={activeAt.y}
+              />
+            )}
+          </AnimatePresence>
+
+          <Tabs.Tab
+            flex={1}
+            unstyled
+            paddingVertical="$2"
+            paddingHorizontal="$3"
+            value="tab1"
+            onInteraction={handleOnInteraction}
+          >
+            <SizableText>Phone Number</SizableText>
+          </Tabs.Tab>
+          <Tabs.Tab
+            flex={1}
+            unstyled
+            paddingVertical="$2"
+            paddingHorizontal="$3"
+            value="tab2"
+            onInteraction={handleOnInteraction}
+          >
+            <SizableText>Email</SizableText>
+          </Tabs.Tab>
+        </Tabs.List>
+      </YStack>
+      <AnimatePresence exitBeforeEnter custom={{ direction }} initial={false}>
+        <AnimatedYStack key={currentTab}>
+          <Tabs.Content value={currentTab} forceMount flex={1} justifyContent="center">
+            <H5 textAlign="center">{currentTab}</H5>
+          </Tabs.Content>
+        </AnimatedYStack>
+      </AnimatePresence>
+    </Tabs>
+  )
+}
+
+const TabsRovingIndicator = ({ active, ...props }: { active?: boolean } & StackProps) => {
+  return (
+    <YStack
+      position="absolute"
+      backgroundColor="white"
+      // opacity={0.7}
+      animation="100ms"
+      enterStyle={{
+        opacity: 0,
+      }}
+      exitStyle={{
+        opacity: 0,
+      }}
+      {...(active && {
+        backgroundColor: '$color8',
+        opacity: 0.6,
+      })}
+      {...props}
+    />
+  )
+}
+
+const AnimatedYStack = styled(YStack, {
+  flex: 1,
+  x: 0,
+  opacity: 1,
+
+  animation: '100ms',
+  variants: {
+    // 1 = right, 0 = nowhere, -1 = left
+    direction: {
+      ':number': (direction) => ({
+        enterStyle: {
+          x: direction > 0 ? -25 : 25,
+          opacity: 0,
+        },
+
+        exitStyle: {
+          zIndex: 0,
+          x: direction < 0 ? -25 : 25,
+          opacity: 0,
+        },
+      }),
+    },
+  } as const,
+})
