fa383856d09dae9d3a8eb60798a1e5fdf5ef6840
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-09-18 15:39:47 +0530

    feat: add gradient button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,16 +1,22 @@
 import { Button, XStack, styled, withStaticProperties } from 'tamagui'
+import { LinearGradient } from 'tamagui/linear-gradient'
 
 export const VButton = styled(Button, {
   width: '100%',
+  fontSize: 18,
+  fontWeight: 600,
 
   variants: {
     type: {
       primary: {
         backgroundColor: '$btn',
         color: 'white',
-        fontSize: 18,
-        fontWeight: 600,
         height: 50,
+      },
+      gradient: {
+        backgroundColor: 'transparent',
+        color: 'white',
+        height: 40,
       },
     },
     size: {
@@ -22,4 +28,17 @@ export const VButton = styled(Button, {
       },
     },
   } as const,
+})
+
+const GradientWrapper = styled(LinearGradient, {
+  borderRadius: "$6",
+  width: '75%',
+  mx: 'auto',
+  colors: ['#C8102E', '#FB4E02'],
+  start: [0, 0.5],
+  end: [0.5, 0],
+})
+
+export const VButtonGradient = withStaticProperties(GradientWrapper, {
+  Button: VButton,
 })
