d2c42d86ec4a6cce18bb8d9b426cb5f145cb883e
Hashan Kurukulasuriya <aeonian.adroit24@gmail.com>
2024-09-20 14:11:18 +0530

    feat: add image upload function

--- app/required-steps/personalInformation.tsx
+++ app/required-steps/personalInformation.tsx
@@ -1,4 +1,4 @@
-import { VButtonGradient } from 'components/ui/Button';
+import { VButton, VButtonGradient } from 'components/ui/Button';
 import { VInput } from 'components/ui/Input';
 import { VText } from 'components/ui/Text';
 import { router } from 'expo-router';
@@ -6,13 +6,15 @@ import { SetStateAction, useMemo, useState } from "react";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Adapt, Image, Select, Separator, Sheet, XStack, YStack } from "tamagui";
 import profileImageUpload from '../../assets/images/profile-upload.png';
+import * as ImagePicker from 'expo-image-picker';
 
 import { Check, ChevronDown } from '@tamagui/lucide-icons';
+import { Pressable } from 'react-native';
 
 
 export default function OtpEnterScreen() {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [date, setDate] = useState(undefined as Date | undefined);
+  const [date, setDate] = useState<Date | undefined>(undefined);
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -22,7 +24,7 @@ export default function OtpEnterScreen() {
     setDatePickerVisibility(false);
   };
 
-  const handleConfirm = (date: SetStateAction<Date>) => {
+  const handleConfirm = (date: SetStateAction<Date | undefined>) => {
     console.info("A date has been picked: ", date);
     setDate(date);
     hideDatePicker();
@@ -33,12 +35,32 @@ export default function OtpEnterScreen() {
     { name: 'Female' },
   ]
 
-  const [val, setVal] = useState(undefined as string | undefined)
+  const [val, setVal] = useState<string | undefined>(undefined)
+
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // https://docs.expo.dev/versions/latest/sdk/imagepicker/
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1], // Square image
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <YStack flex={1} alignItems="center" gap="$5" px="$5" py="$5" bg="white">
       <VText alignSelf="flex-start" type="h3" mb="$2">OTP Verification</VText>
-      <Image source={profileImageUpload} width={160} height={160} objectFit="contain" />
+      <Pressable onPress={pickImage}>
+        <Image source={image ? { uri: image } : profileImageUpload} width={160} height={160} objectFit="contain" borderRadius={12} />
+      </Pressable>
       <VText alignSelf="center" color="$primary" fontWeight={600}>Upload Profile Photo</VText>
       <Separator borderColor="#DADADA" width="75%" />
 
@@ -112,7 +134,7 @@ export default function OtpEnterScreen() {
           <VButtonGradient.Button
             type="gradient"
             onPress={() => {
-              router.navigate('/create-account/otpEnter')
+              router.navigate('./')
             }}
           >
             Save
